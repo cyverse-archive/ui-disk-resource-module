bb6e43c5c5046641e40a72edf46787cacc946386
Jonathan Strootman <jstroot@iplancollaborative.org>
2013-01-29 17:38:23 -0700

    Updates for factoring out irrelevant classes.

--- src/main/java/org/iplantc/core/uidiskresource/client/views/dialogs/SimpleFileUploadDialog.java
+++ src/main/java/org/iplantc/core/uidiskresource/client/views/dialogs/SimpleFileUploadDialog.java
@@ -50,6 +50,8 @@ public class SimpleFileUploadDialog extends IPlantDialog {
     private static final String FORM_HEIGHT = "28";
     public static final String HDN_PARENT_ID_KEY = "dest";
     public static final String HDN_USER_ID_KEY = "user";
+    public static final String FILE_TYPE = "type";
+    public static final String URL_FIELD = "url";
     private static SimpleFileUploadPanelUiBinder BINDER = GWT.create(SimpleFileUploadPanelUiBinder.class);
 
     @UiTemplate("SimpleFileUploadPanel.ui.xml")
