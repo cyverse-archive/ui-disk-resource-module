d5f719e284cd3af5b67633f81af3bb38a3c0dc68
Paul Sarando <psarando@iplantcollaborative.org>
2013-07-16 18:34:31 -0700

    CORE-4374 Lazy loading bug fixes.
    Updated SelectFolderByIdLoadHandler to report an error if a folder in the path it's loading does not exist.
    Removed the SelectFolderByIdLoadHandler#verifyFolderExists method so that this handler lazy-loads as much as it can, then selects that folder, before reporting a "does not exist" error.
    This allows the user to see the parent folder of the folder that no longer exists, since the full path may not fit in the error message.
    Refactored the logic to initialize the paths to lazy-load from SelectFolderByIdLoadHandler#verifyFolderExists into an initPathsToLoad method.
    Refactored SelectFolderByIdLoadHandler to check if the root folders of the Data Window have loaded yet before calling the new initPathsToLoad method (previously, the verifyFolderExists method would cause enough delay when the Data Window initially opened that a root folder check wasn't needed).
    Minor refactor in DiskResourcePresenterImpl by adding a validParentPath method.

--- src/main/java/org/iplantc/core/uidiskresource/client/presenters/proxy/SelectFolderByIdLoadHandler.java
+++ src/main/java/org/iplantc/core/uidiskresource/client/presenters/proxy/SelectFolderByIdLoadHandler.java
@@ -4,17 +4,19 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
+import org.iplantc.core.resources.client.messages.I18N;
+import org.iplantc.core.uicommons.client.info.ErrorAnnouncementConfig;
+import org.iplantc.core.uicommons.client.info.IplantAnnouncer;
 import org.iplantc.core.uicommons.client.models.HasId;
 import org.iplantc.core.uicommons.client.models.diskresources.Folder;
-import org.iplantc.core.uicommons.client.services.DiskResourceServiceFacade;
 import org.iplantc.core.uidiskresource.client.views.DiskResourceView;
 import org.iplantc.core.uidiskresource.client.views.HasHandlerRegistrationMgmt;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
-import com.google.gwt.core.shared.GWT;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.sencha.gxt.data.shared.loader.LoadEvent;
 import com.sencha.gxt.data.shared.loader.LoadHandler;
 
@@ -29,52 +31,59 @@ public final class SelectFolderByIdLoadHandler implements LoadHandler<Folder, Li
 
     private final Stack<String> pathsToLoad = new Stack<String>();
     private final LinkedList<String> path;
-    private boolean folderExists = false;
-    private boolean folderVerifyCalled = false;
+    private boolean rootsLoaded;
 
     private final HasId folderToSelect;
 
     private final DiskResourceView view;
     private final DiskResourceView.Presenter presenter;
     private final HasHandlerRegistrationMgmt regMgr;
-    private final DiskResourceServiceFacade drService;
 
-    public SelectFolderByIdLoadHandler(final HasId folderToSelect, final DiskResourceView.Presenter presenter, final DiskResourceServiceFacade drService) {
+    public SelectFolderByIdLoadHandler(final HasId folderToSelect,
+            final DiskResourceView.Presenter presenter) {
         presenter.maskView();
         this.folderToSelect = folderToSelect;
         this.presenter = presenter;
         this.regMgr = presenter;
         this.view = presenter.getView();
-        this.drService = drService;
+
         // Split the string on "/"
         path = Lists.newLinkedList(Splitter.on("/").trimResults().omitEmptyStrings().split(folderToSelect.getId())); //$NON-NLS-1$
+
+        rootsLoaded = view.getTreeStore().getAllItemsCount() > 0;
+        if (rootsLoaded) {
+            initPathsToLoad();
+        }
     }
 
     @Override
     public void onLoad(LoadEvent<Folder, List<Folder>> event) {
-        // If we haven't verified that the requested folder exists on the server, then do so.
-        if (!folderVerifyCalled) {
-            verifyFolderExists();
-            return;
-        } else if (folderVerifyCalled && !folderExists) {
-            GWT.log("Requested folder could not be found on the server!!" + folderToSelect.getId());
-            unmaskView();
+        if (!rootsLoaded) {
+            initPathsToLoad();
+            rootsLoaded = true;
             return;
         }
 
         // Exit condition
-        if (folderExists && pathsToLoad.isEmpty()) {
+        if (pathsToLoad.isEmpty()) {
             view.setSelectedFolder(event.getLoadConfig());
             unmaskView();
-            return;
-        } else if (folderExists && !pathsToLoad.isEmpty()) {
+        } else {
             path.add(pathsToLoad.pop());
             Folder folder = view.getFolderById("/".concat(Joiner.on("/").join(path)));
             if (folder != null) {
                 // Trigger remote load by expanding folder
                 view.expandFolder(folder);
             } else {
-                GWT.log("Folder not found in view!!");
+                // This handler has loaded as much as it can, but has encountered a folder along the path
+                // that does not exist. Select the last folder loaded, then report the error.
+                String folderName = SafeHtmlUtils.htmlEscape(path.getLast());
+                SafeHtml errMsg = SafeHtmlUtils.fromTrustedString(I18N.ERROR
+                        .diskResourceDoesNotExist(folderName));
+                IplantAnnouncer.getInstance().schedule(new ErrorAnnouncementConfig(errMsg));
+
+                view.setSelectedFolder(event.getLoadConfig());
+                unmaskView();
             }
         }
     }
@@ -83,49 +92,31 @@ public final class SelectFolderByIdLoadHandler implements LoadHandler<Folder, Li
      * Verify if the desired selected folder exists
      * This only needs to occur once.
      */
-    private void verifyFolderExists() {
-        folderVerifyCalled = true;
-        drService.getFolderContents(folderToSelect.getId(), false, new AsyncCallback<String>() {
+    private void initPathsToLoad() {
+        Folder folder = view.getFolderById(folderToSelect.getId());
+        // Find the paths which are not yet loaded, and push them onto the 'pathsToLoad' stack
+        while ((folder == null) && !path.isEmpty()) {
+            pathsToLoad.push(path.removeLast());
+            folder = view.getFolderById("/".concat(Joiner.on("/").join(path))); //$NON-NLS-1$ //$NON-NLS-2$
+        }
 
-            @Override
-            public void onSuccess(String result) {
-                folderExists = true;
-
-                Folder folder = view.getFolderById(folderToSelect.getId());
-                // Find the paths which are not yet loaded, and push them onto the 'pathsToLoad' stack
-                while ((folder == null) && !path.isEmpty()) {
-                    pathsToLoad.push(path.removeLast());
-                    folder = view.getFolderById("/".concat(Joiner.on("/").join(path))); //$NON-NLS-1$ //$NON-NLS-2$
+        if (folder != null) {
+            if (view.isLoaded(folder)) {
+                if (!presenter.getSelectedFolder().equals(folder)) {
+                    view.setSelectedFolder(folder);
                 }
-
-                if (folder != null) {
-                    if (view.isLoaded(folder)) {
-                        if (!presenter.getSelectedFolder().equals(folder)) {
-                            view.setSelectedFolder(folder);
-                        }
-                        unmaskView();
-                    } else {
-                        // Once a valid folder is found in the view, remotely load the
-                        // folder, which will add the next folder in the path to the view's treeStore.
-                        view.expandFolder(folder);
-                    }
-
-                }
-                // If no folders could be found in view
-                if (path.isEmpty()) {
-                    GWT.log("NO ROOT FOLDERS FOUND");
-                    unmaskView();
-                }
-            }
-
-            @Override
-            public void onFailure(Throwable caught) {
-                // TODO If the folder does not exist, inform user..... or something
-                folderExists = false;
-                GWT.log("Time to unregister");
                 unmaskView();
+            } else {
+                // Once a valid folder is found in the view, remotely load the
+                // folder, which will add the next folder in the path to the view's treeStore.
+                view.expandFolder(folder);
             }
-        });
+
+        }
+        // If no folders could be found in view
+        if (path.isEmpty()) {
+            unmaskView();
+        }
     }
 
     private void unmaskView() {
