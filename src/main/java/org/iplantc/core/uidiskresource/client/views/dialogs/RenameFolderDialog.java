cc08b3cc387db8375a33aff0f94d568add591b9d
Jonathan Strootman <jstroot@iplancollaborative.org>
2013-08-03 16:28:25 -0700

    CORE-4015: * NameValidator3 to DiskResourceNameValidator refactor changes

--- src/main/java/org/iplantc/core/uidiskresource/client/views/dialogs/RenameFolderDialog.java
+++ src/main/java/org/iplantc/core/uidiskresource/client/views/dialogs/RenameFolderDialog.java
@@ -2,7 +2,7 @@ package org.iplantc.core.uidiskresource.client.views.dialogs;
 
 import org.iplantc.core.resources.client.messages.I18N;
 import org.iplantc.core.uicommons.client.models.diskresources.Folder;
-import org.iplantc.core.uicommons.client.validators.NameValidator3;
+import org.iplantc.core.uicommons.client.validators.DiskResourceNameValidator;
 import org.iplantc.core.uicommons.client.views.gxt3.dialogs.IPlantPromptDialog;
 import org.iplantc.core.uidiskresource.client.views.widgets.DiskResourceViewToolbar;
 
@@ -12,7 +12,7 @@ import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
 public class RenameFolderDialog extends IPlantPromptDialog {
 
     public RenameFolderDialog(final Folder folder, final DiskResourceViewToolbar.Presenter presenter) {
-        super(I18N.DISPLAY.folderName(), -1, folder.getName(), new NameValidator3());
+        super(I18N.DISPLAY.folderName(), -1, folder.getName(), new DiskResourceNameValidator());
         setHeadingText(I18N.DISPLAY.rename());
         addOkButtonSelectHandler(new SelectHandler() {
 
